bc442078d4c459979144a154cf410990aa7431de
Kyle Anderson <kyle@xkyle.com>
2020-07-12 10:56:59 -0700

    Actually redirect output to readme

--- build_readme.py
+++ build_readme.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    print("Hello World")
+    print("""
+Hello World
+
+![Build README](https://github.com/solarkennedy/solarkennedy/workflows/Build%20README/badge.svg)
+""")
